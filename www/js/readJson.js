646e6c0b226dd2b3a575db7d1b5a54d8d2bdbccc
Robin Vanhoutte <robin_vanhoutte@hotmail.com>
2014-11-14 18:06:48 +0100

    index & boisson
    modif utf-8

--- www/js/readJson.js
+++ www/js/readJson.js
@@ -1,5 +1,5 @@
 //Load categories object JSON
-jQuery.getJSON("menu.json", function(data){         
+jQuery.getJSON("json/menu.json", function(data){         
     // data is yours parsed object
 	alert(data.balance);
 });
