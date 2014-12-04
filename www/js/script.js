c3b6fbe95dc44ac53d99404a222963749c76a407
Florent De Ryck <deryck.florent@gmail.com>
2014-12-04 17:13:18 +0100

    Sandwich
    eeeeeeenfiiiiin putain !
    j'ai bossé deux semaines la dessus !

---
+++ www/js/script.js
@@ -0,0 +1,354 @@
+var result = {
+ 
+    "drinks": [
+ 
+        {
+ 
+            "name": "Chaud fontaine 20Cl",
+ 
+            "price": 1.7
+ 
+        },
+ 
+        {
+ 
+            "name": "Coca cola 20cl",
+ 
+            "price": 1.7
+ 
+        },
+ 
+        {
+ 
+            "name": "Coca cola light 20cl ",
+ 
+            "price": 1.7
+ 
+        },
+        
+        {
+          
+          "name": "Coca cola zero 20cl",
+          "price": 1.7
+          
+        },
+        
+        {
+          "name": "Fanta orange 20cl",
+          "price": 1.7
+          
+        },
+        
+        {
+          
+          "name": "Sprite 20 cl",
+          "price": 1.7
+          
+        },
+        
+        {
+          
+          "name": "Appletiser 20cl",
+          "price": 1.9
+          
+        },
+        
+        {
+          
+          "name": "Gini lemon 20cl",
+          "price": 1.9
+          
+        },
+        
+        {
+          
+          "name": "Gini passion 20cl",
+          "price": 1.9
+          
+        },
+        
+        {
+          
+          "name": "Ice tea (Lipton) 20cl",
+          "price": 1.9
+          
+        },
+        
+        {
+          
+          "name": "Ice tea green (Lipton) 20cl",
+          "price": 1.9
+        
+        },
+        
+        {
+          
+          "name": "Ice tea peche (Lipton) 20cl",
+          "price": 1.9
+          
+        },
+        
+        {
+          
+          "name": "Jus de pomme 20cl",
+          "price": 1.9
+          
+        },
+        
+        {
+          
+          "name": "Jus de pomme cerise 20cl",
+          "price": 1.9
+          
+        },
+        
+        {
+          
+          "name": " Jus de tomate 20cl",
+          "price": 1.9
+          
+        },
+        
+        {
+          
+          "name": "Oasis tropical 20cl",
+          "price": 1.9
+          
+        },
+        
+        {
+          
+          "name": "Schweppes agrum 20cl",
+          "price": 1.9
+          
+        },
+        
+        {
+          
+          "name": "Schweppes tonic 20cl",
+          "price": 1.9
+          
+        }
+        
+    ],
+ 
+    "sandwichs": [
+ 
+        {
+ 
+            "category": "Les Chauds",
+ 
+            "products": [
+ 
+                {
+ 
+                    "name": "De la mort",
+ 
+                    "price": 2.30,
+ 
+                    "composition": ["pain de viande", "gouda", "andalouse"]
+ 
+                },
+ 
+                {
+ 
+                    "name": "Kefta",
+ 
+                    "price": 2.40,
+ 
+                    "composition": ["kefta", "tomate", "oignon", "andalouse"]
+ 
+                },
+                
+                {
+                    "name": "Natacha",
+                    "price": 2.80,
+                    "composition": ["brie", "kefta", "tomate", "andalouse"]
+                    
+                },
+                
+                {
+                    "name": "Ad Hoc",
+                    "price": 3.50,
+                    "composition": ["boeuf", "frite", "cheddar", "oignon", "salade", "tomate"]
+                    
+                }
+ 
+            ]
+        },
+        
+        {
+ 
+            "category": "Les Classiques",
+ 
+            "products": [
+ 
+                {
+ 
+                    "name": "British", 
+                    "price": 2.40, 
+                    "composition": ["bacon", "oeufs", "salade", "mayonnaise"]
+ 
+                },
+ 
+                {                    
+ 
+                    "name": "Nazareth", 
+                    "price": 2.40, 
+                    "composition" : ["poulet", "salade" ,"olive", "mayonnaise" , "pesto"]
+ 
+                },
+                
+                 {
+ 
+                    "name": "Turc", 
+                    "price": 2.40, 
+                    "composition": ["gyros", "ail" , "salade", "tomate"]
+ 
+                },
+                
+                 {
+ 
+                    "name": "USA", 
+                    "price": 2.40, 
+                    "composition": ["americain", "oeuf", "cornichon" ]
+ 
+                },
+                
+                 {
+ 
+                    "name": "Crabs", 
+                    "price": 2.40, 
+                    "composition": ["crabe", "salade", "Tomate", "mayonnaise"]
+ 
+                },
+                
+                 {
+ 
+                    "name": "Emma", 
+                    "price": 2.50, 
+                    "composition": ["poulet", "pane", "chevre", "pesto"]
+ 
+                },
+                
+                 {
+ 
+                    "name": "Mexicain", 
+                    "price": 2.50, 
+                    "composition": ["poulet", "poivrons", "mais", "salade", "salsa"]
+                     
+                },
+                
+                 {
+ 
+                    "name": "Nausica", 
+                    "price": 2.50, 
+                    "composition": ["falafel", "aubergines", "tomates", "coriandre"]
+ 
+                },
+                
+                 {
+ 
+                    "name": "Honolulu", 
+                    "price": 2.60, 
+                    "composition": ["poulet", "ananas", "mais", "tomate", "cocktail"]
+ 
+                },
+                
+                 {
+ 
+                    "name": "Italien", 
+                    "price": 2.60, 
+                    "composition": ["jambon", "italien", "mozzarela", "pesto", "tomate"]
+ 
+                },
+                
+               {
+ 
+                    "name": "Biquette", 
+                    "price": 2.70, 
+                    "composition": ["chevre", "lardons", "salade", "miel"]
+ 
+                },
+                
+                 {
+ 
+                    "name": "Ocean", 
+                    "price": 2.70, 
+                    "composition": ["saumon", "concombre", "fromage blanc"]
+ 
+                },
+                
+                 {
+ 
+                    "name": "Pepper", 
+                    "price": 2.70, 
+                    "composition": ["poulet", "pane", "cheddar", "salade", "tomates", "poivre"]
+ 
+                },
+                
+                 {
+ 
+                    "name": "Athena", 
+                    "price": 2.80, 
+                    "composition": ["feta", "concombres", "olives", "tomates"]
+ 
+                },
+                
+                 {
+ 
+                    "name": "Oriental", 
+                    "price": 2.90, 
+                    "composition": ["falafel", "houmous", "olives", "tomates"]
+ 
+                },
+                
+                 {
+ 
+                    "name": "Club Poulet", 
+                    "price": 2.90, 
+                    "composition": ["gouda", "poulet", "mayonnaise", "oeuf", "salade", "tomate"]
+ 
+                },
+                
+                 {
+ 
+                    "name": "Club", 
+                    "price": 2.90, 
+                    "composition": ["gouda", "jambon", "cuit", "mayonnaise", "oeuf", "salade", "tomate"]
+ 
+                },
+                
+                 {
+ 
+                    "name": "Toscan", 
+                    "price": 3.00, 
+                    "composition": ["carpaccio", "roquette", "tomate", "parmesan", "pesto"]
+ 
+                      }
+ 
+             ]
+ 
+        }
+ 
+    ]
+ 
+};
+    
+    $("div").append('<h1>Les Boissons</h1>');
+  $.each(result.drinks, function(nom, drink) {
+      $("div").append('<strong>'+drink.name+'</strong>: '+drink.price+'€<br />');
+  });
+   
+  $("div").append('<h1>Les Sandwichs</h1>');
+  $.each(result.sandwichs, function(nom, sandwich) {
+     
+    $("div").append('<h2>'+sandwich.category+'</h2>');
+     
+    $.each(sandwich.products, function(i, product) {
+       
+      $("div").append('<strong>'+product.name+'</strong> '+product.price+'€ <i>('+product.composition+')</i><br />');
+       
+    });
+     
+  });
