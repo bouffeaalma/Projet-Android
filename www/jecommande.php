2de5ed8688a05f4ce4c559fb97c9375774e73247
Alexis Neirinckx <alex.beek@hotmail.com>
2014-12-09 10:41:50 +0100

    modifs envoie mail

---
+++ www/jecommande.php
@@ -0,0 +1,22 @@
+﻿<?php 
+$formNom = $_POST['nom']; 
+
+if(!empty($formNom)) 
+{ 
+
+$MessageInfo='Nom:'.utf8_decode($formNom)."\n"; 
+
+if( mail('commanderaalma@gmail.com','Message',$MessageInfo) ) 
+{ 
+echo 'les informations ont bien été envoyées!'; 
+} 
+else 
+{ 
+echo 'une erreur est survenue lors de l\'envoi du message'; } 
+} 
+else 
+{ 
+//il y a des champs vide 
+echo 'merci de remplir le champs nom'; 
+} 
+?> 
